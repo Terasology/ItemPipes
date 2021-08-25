276bafdc56ba6d4b7398b582ab7a6799a9bd587d
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 18:58:59 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#40)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/itempipes/components/PipeComponent.java
+++ src/main/java/org/terasology/itempipes/components/PipeComponent.java
@@ -1,9 +1,14 @@
-// Copyright 2020 The Terasology Foundation
+// Copyright 2021 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.itempipes.components;
 
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 
-public class PipeComponent implements Component {
+public class PipeComponent implements Component<PipeComponent> {
     public float friction = .1f;
+
+    @Override
+    public void copyFrom(PipeComponent other) {
+        this.friction = other.friction;
+    }
 }
