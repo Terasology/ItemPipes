4118bd6415e7e1e49be5130f27ea954f59621ac1
darkweird <foxflameless@gmail.com>
2021-09-09 11:18:57 +0300

    feature(gestaltv7-events): replace ReceiveEvent with gestaltv7's ReceiveEvent

--- src/main/java/org/terasology/itempipes/action/RandomMappingAction.java
+++ src/main/java/org/terasology/itempipes/action/RandomMappingAction.java
@@ -3,11 +3,11 @@
 package org.terasology.itempipes.action;
 
 import org.terasology.engine.entitySystem.entity.EntityRef;
-import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
 import org.terasology.engine.math.Side;
+import org.terasology.gestalt.entitysystem.event.ReceiveEvent;
 import org.terasology.itempipes.components.PipeRandomMappingComponent;
 import org.terasology.itempipes.event.PipeMappingEvent;
 
